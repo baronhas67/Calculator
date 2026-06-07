432fc7dee244af996ac3b9dd17b0d263e471fc9b
Bhagath <baronhas67@gmail.com>
2026-06-07 15:47:37 +0530

    Take the input and display results on "display"
    added an event listener to the main div and change
    the display text content when clicked.

--- engine.js
+++ engine.js
@@ -14,6 +14,9 @@ function divide(a, b) {
     a / b;
 }
 
+const buttonsDiv = document.querySelector("#buttons-div");
+const display = document.querySelector("#display");
+
 let operator;
 let num1;
 let num2;
@@ -37,3 +40,20 @@ function operate(a, b, op) {
         break;
     }
 }
+
+buttonsDiv.addEventListener("click", (e) => {
+    const clickedBtn = e.target.id;
+
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    if (isFinite(clickedBtn)) {
+        num1 = arr[+clickedBtn];
+    }
+
+     display.textContent = num1;
+    })
+
+function changeAndDisplayResults() {
+    
+}
+
